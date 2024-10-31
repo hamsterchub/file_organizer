c4af86201453f9b8d2a05999fb9fa460e7c53f99
hamsterchub <89563112+hamsterchub@users.noreply.github.com>
2024-10-30 21:54:16 -0500

    Finally source controlling
    First commit

---
+++ file_organizer.py
@@ -0,0 +1,115 @@
+import os
+import shutil
+
+# Import project files
+import util_functions
+from handler_functions import get_file_type_handler
+
+
+# Main program logic
+# Inputs:
+#       str - Directory to parse for files. May either be the directory defined in the __main__ function or a nested folder, parsed recursively
+#       str - Parent directory to move files to. Most if not all files will be moved into a subdirectory under here
+#           Only defined in __main__. Not meant to be redefined by recursive calls
+#       bool - Indicate if the function is running recursively. Set to True explicitly by recursive call inside this function
+#           This is used to avoid deleting the source input directory defined in __main__ since the program will attempt to delete input_dir if it's empty
+#       int - Running count of files processed. Need this due to the recursion. NOT mission critical functionality
+#       int - Running count of directories processed. Need this due to the recursion. NOT mission critical functionality
+#       int - Running count of errors encountered. Need this due to the recursion. NOT mission critical functionality
+# Outputs:
+#       [int, int, int] - To be used by scoreboard printer functionality or append after recursive run. NOT mission critical functionality
+def run_main(input_dir, output_dir, is_recursive = False, num_files_processed = 0, num_folders_removed = 0, num_errors = 0):
+    
+    # Check if the input directory doesn't exist
+    if not os.path.exists(input_dir):
+        print(f"Error - The input directory doesn't exist! Either the input_dir in the __main__ function is wrong or a folder deletion caused an issue. Exiting main")
+        return [num_files_processed, num_folders_removed, num_errors]
+    
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        print(f"Action - The output directory doesn't exist. Creating \"{output_dir}\"")
+        os.makedirs(output_dir)
+
+    # Loop through all files in the input directory
+    for filename in os.listdir(input_dir):
+        file_path = os.path.join(input_dir, filename)
+        print(f"Log - Current file path being analyzed: \"{file_path}\"")
+              
+        # Get the correct handler function for the extension of the current path (or 0 if current path is a folder)
+        file_type_handler = get_file_type_handler(file_path)
+
+        # Check if file_path is a directory (return code 0 from get_file_type_handler function). If so, recursively call run_main
+        # Since the function is recursive, need to add the running counts from nested runs to the current run
+        if file_type_handler == 0:
+            main_out = run_main(file_path, output_dir, True, num_files_processed, num_folders_removed, num_errors)
+            num_files_processed += main_out[0]
+            num_folders_removed += main_out[1]
+            num_errors += main_out[2]
+            continue
+        print(f"Log - \"{file_path}\" is NOT a directory")
+        
+        # Get the output path to be appended to output_dir from the handler function returned by the get_file_type_handler function
+        partial_output_path = file_type_handler(file_path)
+        print(f"Log - Partial output path of \"{file_path}\" is \"{partial_output_path}\"")
+
+        # Catch error from handler functions
+        if partial_output_path == 1:
+            print(f"Error - Caught error from file handler. Will not process this file: \"{file_path}\"")
+            num_errors += 1
+            continue
+        print(f"Log - No error caught from handler function during processing of \"{file_path}\"")
+        
+        # Get full output directory and create if doesn't exist
+        create_out_dir_return = util_functions.create_output_dir(output_dir, partial_output_path)
+        if (create_out_dir_return == 0):    # There was an error in creating the output directory
+            num_errors += 1
+            continue
+        print(f"Log - No error in creating output directory \"{create_out_dir_return}\"")
+
+        # Check to see if the current file already exists in the appropriate destination folder and attempt to delete it
+        bool_find_dupes_out = util_functions.find_dupes(create_out_dir_return, filename)
+        if not bool_find_dupes_out: # File was not successfully deleted
+            num_errors += 1
+            continue
+        print(f"Log - No error in deleting \"{file_path}\"")
+        
+        # Move the file to the full output path subdirectory and increment successful procs by 1
+        shutil.move(file_path, create_out_dir_return)
+        print(f"Action - Moved: \"{filename}\" to \"{create_out_dir_return}\"\n")
+        num_files_processed += 1
+
+    # Checks for empty input directory. Delete the input_dir unless is_recursive is set to FALSE
+    if util_functions.is_dir_empty(input_dir):        
+        if is_recursive:
+            bool_cleanup_output = util_functions.remove_empty_dir(input_dir) # False if cleanup had an error, True otherwise
+
+            # Increment correct stat based on cleanup function return value
+            if bool_cleanup_output:
+                num_folders_removed += 1
+            else:
+                num_errors += 1
+
+            return [num_files_processed, num_folders_removed, num_errors]
+
+        # This will be executed if input_dir is empty and is_recursive is FALSE
+        print(f"Log - The input directory for the program is empty")
+        return [num_files_processed, num_folders_removed, num_errors]
+
+    # If there's issues processing 1+ files in a directory, you'll hit this
+    print(f"Log - There's at least 1 file still in \"{input_dir}\"")
+    print(f"Log - User needs to manually investigate remaining file(s) in \"{input_dir}\"")
+    return [num_files_processed, num_folders_removed, num_errors]
+
+
+# Function to be called upon program invocation
+if __name__ == "__main__":
+
+    # Set input and output directories
+    input_dir = "E:\Development\Python\Input"
+    output_dir = "E:\Development\Python\Output"
+    print(f"Log - Input directory: \"{input_dir}\"\nLog - Output directory: \"{output_dir}\"")
+
+    print(f"Log - Entering main\n")
+    main_out = run_main(input_dir, output_dir)
+    print(f"Log - Program complete!")
+    util_functions.print_scoreboard(main_out[0], main_out[1], main_out[2])
---
+++ image_handler.py
@@ -0,0 +1,99 @@
+from datetime import datetime
+
+# Used to get image metadata
+from PIL import Image
+from PIL. ExifTags import TAGS
+
+
+# Debugging function to parse exif data and print to console if there's some pics being annoying
+# Inputs:
+#       exif_data object - exif data of an image
+# Outputs:
+#       None
+def print_exif_data(exif_data):
+    print(f"\n####################################")
+    for tag, value in exif_data.items():
+        tag_name = TAGS.get(tag, tag)
+        print(f"\"{tag_name}\": \"{value}\"")
+    print(f"####################################\n")
+    return
+
+
+# Logic to determine which exif data we want to organize our file by
+# Inputs:
+#       [<date obj>, <date obj>] - Dates to consider using for organizing an image file
+# Return:
+#       <date obj> - The chosen date object for organizing the image
+def choose_exif_date(dates):
+    chosen_date = min(dates)
+    print(f"Chosen exif date for current file is \"{chosen_date}\"")
+    return chosen_date
+
+
+# Return directory structure in Year-Month-Day format given the metadata of an image for creation date/time
+# Inputs:
+#       exif_data object - exif data of an image to get various date/time metadata info on it
+# Outputs:
+#       str - "<yyyy>-<mm>-<dd>"
+def get_image_exif_output(img_exif_data):
+    date_original = img_exif_data.get(36867)   # DateTimeOriginal tag
+    date_modified = img_exif_data.get(36866)   # DateModified tag
+
+    # Setting up local variables to use for parsing choise exif metadata fields. Setting defalt date time to year far, far away for comparison
+    bool_is_date_orig_or_mod = False    # Variable to be turned if either date_original or date_modified are valid
+    date_orig_extract = datetime(9999, 1, 1, 1, 1, 1)
+    date_mod_extract = datetime(9999, 1, 1, 1, 1, 1)
+
+    if date_original:
+        bool_is_date_orig_or_mod = True
+        date_orig_extract = datetime.strptime(date_original, "%Y:%m:%d %H:%M:%S")
+        print(f"Log - There is a DateTimeOriginal tag on this file: \"{date_orig_extract}\"")
+    else:
+        print(f"Log - There is NOT a DateTimeOriginal tag on this file")
+        
+    if date_modified:
+        bool_is_date_orig_or_mod = True
+        date_mod_extract = datetime.strptime(date_modified, "%Y:%m:%d %H:%M:%S")
+        print(f"Log - There is a Modified Date tag on this file: \"{date_mod_extract}\"")
+    else:
+        print(f"Log - There is NOT a Modified Date tag on this file")
+
+    if bool_is_date_orig_or_mod:
+        # Return the appropriate date in yyyy-mm-dd format based on the logic of the choose_exif_date function
+        return choose_exif_date([date_orig_extract, date_mod_extract]).strftime("%Y-%m-%d")
+
+    print(f"Log - There is NOT a DateTimeOriginal nor a DateModified tag on this file")
+    return "no_date_time_tags"
+
+
+# Set output directory appropriately for images (including error conditions)
+# Inputs:
+#       str - Full path to an image file
+# Outputs:
+#       str - Desired subdirectory in the program output directory (defined in __main__) to drop files with an image type file extension
+#           ex. "images/2000-01-01"
+def get_image_output_path(image_path):
+    img_out_path = "images/"
+    try:
+        # Open the media to get its creation date
+        img = Image.open(image_path)
+        exif_data = img._getexif()
+
+        # Get the creation date from EXIF data if available
+        if exif_data is not None:
+            print(f"Log - There is valid exif data on this image")
+
+            ##########################
+            print_exif_data(exif_data)
+            ##########################
+            
+            return img_out_path + get_image_exif_output(exif_data)
+
+        # This code will execute if EXIF data is NOT available
+        print(f"Log - There is NOT valid exif data on this image")
+        return img_out_path + "no_exif_data"
+
+    # This code will execute if there was an issue opening the file
+    except Exception as e:
+        print(f"Error - Error while opening \"{image_path}\": {e}")
+        return 1
